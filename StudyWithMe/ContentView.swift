4203cc1f9688c1d243f01f6f06d958d19c10bedb
gustavo2795 <gustavo.unb27@gmail.com>
2024-09-06 10:18:53 -0300

    Initial Commit

---
+++ StudyWithMe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StudyWithMe
+//
+//  Created by Gustavo Braz on 06/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StudyWithMe/StudyWithMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyWithMeApp.swift
+//  StudyWithMe
+//
+//  Created by Gustavo Braz on 06/09/24.
+//
+
+import SwiftUI
+
+@main
+struct StudyWithMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
