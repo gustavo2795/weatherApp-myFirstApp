5ad4f1f6f60535513dda534adfb4e9900a5dff22
gustavo2795 <gustavo.unb27@gmail.com>
2024-09-09 18:18:52 -0300

    Finish UI with button and refact gradient

--- StudyWithMe/ContentView.swift
+++ StudyWithMe/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-            LinearGradient(gradient: Gradient(colors: [Color.blue, Color.white]), startPoint: .topLeading, endPoint: .bottomTrailing)
+            LinearGradient(gradient: Gradient(colors: [Color.blue, Color("lightBlue")]), startPoint: .topLeading, endPoint: .bottomTrailing)
                 .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
             VStack {
                 Text("Luziânia, GO")
@@ -29,14 +29,26 @@ struct ContentView: View {
                         .font(.system(size: 70, weight: .medium))
                         .foregroundStyle(.white)
                 }
-                Spacer()
-                HStack {
-                    WeatherDayView(dayOfWeek: "MON", weatherImage: "cloud.sun.fill", value: 40)
+                .padding(.bottom, 50)
+
+                HStack(spacing: 10) {
+                    WeatherDayView(dayOfWeek: "MON", weatherImage: "cloud.sun.fill", value: 36)
                     WeatherDayView(dayOfWeek: "TUE", weatherImage: "sun.max.fill", value: 40)
-                    WeatherDayView(dayOfWeek: "WED", weatherImage: "cloud.bolt.fill", value: 40)
-                    WeatherDayView(dayOfWeek: "THU", weatherImage: "cloud.heavyrain.fill", value: 40)
-                    WeatherDayView(dayOfWeek: "FRI", weatherImage: "cloud.sun.fill", value: 40)
-                    WeatherDayView(dayOfWeek: "SAT", weatherImage: "cloud.sun.fill", value: 40)
+                    WeatherDayView(dayOfWeek: "WED", weatherImage: "cloud.bolt.fill", value: 28)
+                    WeatherDayView(dayOfWeek: "THU", weatherImage: "cloud.heavyrain.fill", value: 22)
+                    WeatherDayView(dayOfWeek: "FRI", weatherImage: "cloud.sun.fill", value: 28)
+                    WeatherDayView(dayOfWeek: "SAT", weatherImage: "cloud.sun.fill", value: 30)
+                }
+                Spacer()
+                
+                Button {
+                    print("tapped")
+                } label: {
+                    Text("Change Day time")
+                        .frame(width: 280, height: 50)
+                        .background(Color.white)
+                        .font(.system(size: 20, weight: .bold))
+                        .cornerRadius(10)
                 }
                 Spacer()
             }
